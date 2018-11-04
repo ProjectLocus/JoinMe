1b43c85e0e2da9e3ae8c918cfe297d42b2a3864b
bBleck <bmbleck@hotmail.com>
2018-11-04 15:13:03 -0700

    Added toolbar, added fragment frame in main xml

--- app/src/main/java/edu/cnm/deepdive/joinme/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    initDB();
+    initViews();
+    initFields();
+
+  }
+
+  private void initFields() {
+  }
+
+  private void initViews() {
+  }
+
+  private void initDB() {
   }
 }
