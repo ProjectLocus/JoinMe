46efea7cd62d057cc8585992419ce725f7877d3c
bBleck <bmbleck@hotmail.com>
2018-11-29 14:59:53 -0700

    De minimis changes.

--- app/src/main/java/edu/cnm/deepdive/joinme/model/entity/Person.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/model/entity/Person.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.joinme.model.entity;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
