1906bec0d54a78082e373587bd333e63d9738af2
bBleck <bmbleck@hotmail.com>
2018-11-29 09:51:15 -0700

    Commented out DB so it builds.

--- app/src/main/java/edu/cnm/deepdive/joinme/model/db/ClientDB.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/model/db/ClientDB.java
@@ -8,16 +8,16 @@ import edu.cnm.deepdive.joinme.model.entity.Invitation;
 import edu.cnm.deepdive.joinme.model.entity.Person;
 
 
-/**
- * Class that defines the {@link Database}.
- */
-@Database(
-    entities = {Invitation.class, Person.class},
-    version = 1,
-    exportSchema = true
-)
+///**
+// * Class that defines the {@link Database}.
+// */
+//@Database(
+//    entities = {Invitation.class, Person.class},
+//    version = 1,
+//    exportSchema = true
+//)
 @TypeConverters(Converters.class)
-public abstract class ClientDB extends RoomDatabase {
+public abstract class ClientDB  {
 
   private static final String DB_NAME = "client_db";
 
