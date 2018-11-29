eab2bdf5407876e915dcf6a1e9bae7e05484254e
bBleck <bmbleck@hotmail.com>
2018-11-29 05:34:03 -0700

    Added javadocs comments to Person.class

--- app/src/main/java/edu/cnm/deepdive/joinme/model/entity/Person.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/model/entity/Person.java
@@ -10,6 +10,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * The type Person.
+ */
 @Entity
 public class Person {
 
@@ -118,142 +121,312 @@ public class Person {
   //***************** GETTERS AND SETTERS *******************************************************//
 
 
+  /**
+   * Gets latitude.
+   *
+   * @return the latitude
+   */
   public double getLatitude() {
     return latitude;
   }
 
+  /**
+   * Sets latitude.
+   *
+   * @param latitude the latitude
+   */
   public void setLatitude(double latitude) {
     this.latitude = latitude;
   }
 
+  /**
+   * Gets longitude.
+   *
+   * @return the longitude
+   */
   public double getLongitude() {
     return longitude;
   }
 
+  /**
+   * Sets longitude.
+   *
+   * @param longitude the longitude
+   */
   public void setLongitude(double longitude) {
     this.longitude = longitude;
   }
 
+  /**
+   * Gets person id.
+   *
+   * @return the person id
+   */
   @NonNull
   public UUID getPersonId() {
     return personId;
   }
 
+  /**
+   * Sets person id.
+   *
+   * @param personId the person id
+   */
   public void setPersonId(@NonNull UUID personId) {
     this.personId = personId;
   }
 
+  /**
+   * Gets display name.
+   *
+   * @return the display name
+   */
   @NonNull
   public String getDisplayName() {
     return displayName;
   }
 
+  /**
+   * Sets display name.
+   *
+   * @param displayName the display name
+   */
   public void setDisplayName(@NonNull String displayName) {
     this.displayName = displayName;
   }
 
+  /**
+   * Gets user image location.
+   *
+   * @return the user image location
+   */
   public String getUserImageLocation() {
     return userImageLocation;
   }
 
+  /**
+   * Sets user image location.
+   *
+   * @param userImageLocation the user image location
+   */
   public void setUserImageLocation(String userImageLocation) {
     this.userImageLocation = userImageLocation;
   }
 
+  /**
+   * Gets user description.
+   *
+   * @return the user description
+   */
   public String getUserDescription() {
     return userDescription;
   }
 
+  /**
+   * Sets user description.
+   *
+   * @param userDescription the user description
+   */
   public void setUserDescription(String userDescription) {
     this.userDescription = userDescription;
   }
 
+  /**
+   * Is this me boolean.
+   *
+   * @return the boolean
+   */
   public boolean isThisMe() {
     return isThisMe;
   }
 
+  /**
+   * Sets this me.
+   *
+   * @param thisMe the this me
+   */
   public void setThisMe(boolean thisMe) {
     isThisMe = thisMe;
   }
 
+  /**
+   * Gets first name.
+   *
+   * @return the first name
+   */
   public String getFirstName() {
     return firstName;
   }
 
+  /**
+   * Sets first name.
+   *
+   * @param firstName the first name
+   */
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
 
+  /**
+   * Gets last name.
+   *
+   * @return the last name
+   */
   public String getLastName() {
     return lastName;
   }
 
+  /**
+   * Sets last name.
+   *
+   * @param lastName the last name
+   */
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
 
+  /**
+   * Gets user b day.
+   *
+   * @return the user b day
+   */
   public Date getUserBDay() {
     return userBDay;
   }
 
+  /**
+   * Sets user b day.
+   *
+   * @param userBDay the user b day
+   */
   public void setUserBDay(Date userBDay) {
     this.userBDay = userBDay;
   }
 
+  /**
+   * Gets user email.
+   *
+   * @return the user email
+   */
   public String getUserEmail() {
     return userEmail;
   }
 
+  /**
+   * Sets user email.
+   *
+   * @param userEmail the user email
+   */
   public void setUserEmail(String userEmail) {
     this.userEmail = userEmail;
   }
 
+  /**
+   * Gets user password.
+   *
+   * @return the user password
+   */
   public String getUserPassword() {
     return userPassword;
   }
 
+  /**
+   * Sets user password.
+   *
+   * @param userPassword the user password
+   */
   public void setUserPassword(String userPassword) {
     this.userPassword = userPassword;
   }
 
+  /**
+   * Gets invites sent.
+   *
+   * @return the invites sent
+   */
   public List<Invitation> getInvitesSent() {
     return invitesSent;
   }
 
+  /**
+   * Sets invites sent.
+   *
+   * @param invitesSent the invites sent
+   */
   public void setInvitesSent(List<Invitation> invitesSent) {
     this.invitesSent = invitesSent;
   }
 
+  /**
+   * Gets invites received.
+   *
+   * @return the invites received
+   */
   public List<Invitation> getInvitesReceived() {
     return invitesReceived;
   }
 
+  /**
+   * Sets invites received.
+   *
+   * @param invitesReceived the invites received
+   */
   public void setInvitesReceived(
       List<Invitation> invitesReceived) {
     this.invitesReceived = invitesReceived;
   }
 
+  /**
+   * Gets invites created.
+   *
+   * @return the invites created
+   */
   public List<Invitation> getInvitesCreated() {
     return invitesCreated;
   }
 
+  /**
+   * Sets invites created.
+   *
+   * @param invitesCreated the invites created
+   */
   public void setInvitesCreated(
       List<Invitation> invitesCreated) {
     this.invitesCreated = invitesCreated;
   }
 
+  /**
+   * Gets distance to user.
+   *
+   * @return the distance to user
+   */
   public double getDistanceToUser() {
     return distanceToUser;
   }
 
+  /**
+   * Sets distance to user.
+   *
+   * @param distanceToUser the distance to user
+   */
   public void setDistanceToUser(double distanceToUser) {
     this.distanceToUser = distanceToUser;
   }
 
+  /**
+   * Gets user image.
+   *
+   * @return the user image
+   */
   public Drawable getUserImage() {
     return userImage;
   }
 
+  /**
+   * Sets user image.
+   *
+   * @param userImage the user image
+   */
   public void setUserImage(Drawable userImage) {
     this.userImage = userImage;
   }
