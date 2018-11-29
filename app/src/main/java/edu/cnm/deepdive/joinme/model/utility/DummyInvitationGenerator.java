8b1645fcd0e7b937411a84261ac18c110fc8fbc5
bBleck <bmbleck@hotmail.com>
2018-11-29 08:08:56 -0700

    Added dummy person generator class, started on dummy invitation generator.

---
+++ app/src/main/java/edu/cnm/deepdive/joinme/model/utility/DummyInvitationGenerator.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.joinme.model.utility;
+
+import edu.cnm.deepdive.joinme.model.entity.Invitation;
+import java.util.List;
+
+public class DummyInvitationGenerator {
+
+  public static List<Invitation> getXDummyInvitations()
+
+}
