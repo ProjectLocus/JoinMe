4e300d63b67193e87a21cf79c9c4d3707420e2c2
Dina <dmrabanal@gmail.com>
2018-11-29 18:06:54 -0700

    add adapters.

--- app/src/main/java/edu/cnm/deepdive/joinme/view/PeopleAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/view/PeopleAdapter.java
@@ -1,25 +1,18 @@
 package edu.cnm.deepdive.joinme.view;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.net.wifi.p2p.WifiP2pManager.DnsSdTxtRecordListener;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import de.hdodenhof.circleimageview.CircleImageView;
 import edu.cnm.deepdive.joinme.R;
 import edu.cnm.deepdive.joinme.model.entity.Invitation;
+import edu.cnm.deepdive.joinme.model.entity.Person;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Describes an adapter for a {@link RecyclerView}.
@@ -28,10 +21,10 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.Holder> {
 
   private static final String TAG = "SearchFragAdapter";
 
-  private List<Invitation> listForRecycler;
+  private List<Person> listForRecycler;
   private Activity activity;
 
-  public PeopleAdapter(Activity activity, List<Invitation> listForRecycler) {
+  public PeopleAdapter(Activity activity, List<Person> listForRecycler) {
     this.activity = activity;
     this.listForRecycler = listForRecycler;
   }
@@ -47,10 +40,15 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.Holder> {
 
   @Override
   public void onBindViewHolder(@NonNull final Holder holder, int position) {
-    //TODO add image for invitation with Glide/Picasson implementation.
-    holder.comments.setText(listForRecycler.get(position).getDescription());
-
+    int[] picRes = {R.drawable.alex,R.drawable.brian,R.drawable.notdeb,R.drawable.johnrow,R.drawable.lily,R.drawable.weenie};
+    Random rng = new Random();
+    holder.image.setImageResource(picRes[rng.nextInt(picRes.length-1)]);
+    //holder.name.setText(listForRecycler.get(position).getFirstName());
+    //TODO fix error for above execution: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.TextView.setText(java.lang.CharSequence)' on a null object reference
+    holder.comments.setText("Lorem Ipsum");
   }
+
+
 
   @Override
   public int getItemCount() {
@@ -63,7 +61,7 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.Holder> {
   class Holder extends RecyclerView.ViewHolder {
 
     private CircleImageView image;
-    private TextView title;
+    private TextView name;
     private TextView comments;
 
     /**
@@ -73,9 +71,9 @@ public class PeopleAdapter extends RecyclerView.Adapter<PeopleAdapter.Holder> {
      */
     public Holder(@NonNull View itemView) {
       super(itemView);
-      image = itemView.findViewById(R.id.civ_rv_item_invitationlist_profile_image);
-      title = itemView.findViewById(R.id.tv_frag_invitation_rv_title);
-      comments = itemView.findViewById(R.id.tv_rv_item_invitationlist_comments);
+      image = itemView.findViewById(R.id.civ_rv_item_person_profile_image);
+      name = itemView.findViewById(R.id.tv_frag_people_rv_title);
+      comments = itemView.findViewById(R.id.tv_rv_item_person_comment);
     }
   }
 }
