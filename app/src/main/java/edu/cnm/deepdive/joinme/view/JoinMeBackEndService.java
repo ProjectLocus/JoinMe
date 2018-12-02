97a1f20c6f6c4095a6795f9e05d971c5f697397b
Alex Rael <alprael22@gmail.com>
2018-12-02 16:10:00 -0700

    updated join me service class

--- app/src/main/java/edu/cnm/deepdive/joinme/view/JoinMeBackEndService.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/view/JoinMeBackEndService.java
@@ -1,9 +1,11 @@
 package edu.cnm.deepdive.joinme.view;
 
+import edu.cnm.deepdive.joinme.model.entity.Invitation;
 import edu.cnm.deepdive.joinme.model.entity.Person;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
@@ -12,15 +14,160 @@ import retrofit2.http.Path;
 
 public interface JoinMeBackEndService {
 
-  @GET("vertices/people")
-  Call<List<Person>> getAllVerticiesPeople(@Header("Authorization") String authorization);
+  /**
+   * Gets all People
+   * @param authorization
+   * @return
+   */
+  @GET("people")
+  Call<List<Person>> getAllPeople(@Header("Authorization") String authorization);
 
-  @PUT("person/{personId}")
+  /**
+   * Gets a certain person by their ID
+   * @param authorization
+   * @param personId
+   * @param person
+   * @return
+   */
+  @GET("people/{personId}")
+  Call<Person> getPerson(@Header("Authorization") String authorization,
+      @Path("personId") int personId, @Body Person person);
+
+  /**
+   * Updates a certain person by their ID
+   * @param authorization
+   * @param personId
+   * @param person
+   * @return
+   */
+  @PUT("people/{personId}")
   Call<Person> updatePerson(@Header("Authorization") String authorization,
       @Path("personId") int personId, @Body Person person);
 
-  @POST("person")
+  /**
+   * Creates a new person
+   * @param authorization
+   * @param person
+   * @return
+   */
+  @POST("people")
   Call<Person> addPerson(@Header("Authorization") String authorization, @Body Person person);
 
+  /**
+   * Deletes a certain person
+   * @param authorization
+   * @return
+   */
+  @DELETE("people/{personId}")
+  Call<Person> deletePerson(@Header("Authorization") String authorization,
+      @Path("personId") int personId);
 
+  /**
+   * Gets all people near a person
+   * @param authorization
+   * @param person
+   * @param personId
+   * @return
+   */
+  @GET("people/{personId}/people")
+  Call<List<Person>> getAllPeopleNearPerson(@Header("Authorization") String authorization,
+      @Body Person person, @Path("personId") int personId);
+
+  /**
+   * Gets all invitations per user by their ID
+   * @param Authorization
+   * @param personId
+   * @param person
+   * @param invitation
+   * @return
+   */
+  @GET("people/{personId}/invitation")
+  Call<List<Invitation>> getAllInvitationsPerPerson(@Header("Authorization") String Authorization,
+      @Path("personId") int personId, @Body Person person, @Body Invitation invitation);
+
+  /**
+   * Gets a specific invitation by its ID, that a certain person created
+   * @param authorization
+   * @param personId
+   * @param person
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @GET("people/{personId}/invitation/{invitationId}")
+  Call<Invitation> getInvitationPerPerson(@Header("Authorization") String authorization,
+      @Path("personId") int personId, @Body Person person, @Path("invitationId") int invitationId,
+      @Body Invitation invitation);
+
+  /**
+   * Creates a new invitation by the person
+   * @param authorization
+   * @param personId
+   * @param person
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @POST("people/{personId}/invitation")
+  Call<Invitation> addInvitation(@Header("Authorization") String authorization,
+      @Path("personId") int personId, @Body Person person, @Path("invitationId") int invitationId,
+      @Body Invitation invitation);
+
+  /**
+   * Updates a specific invitation, that a person created
+   * @param authorization
+   * @param personId
+   * @param person
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @PUT("people/{personId}/invitation/{invitationId}")
+  Call<Invitation> updateInvitation(@Header("Authorization") String authorization,
+      @Path("personId") int personId, @Body Person person, @Path("invitationId") int invitationId,
+      @Body Invitation invitation);
+
+  /**
+   * Deletes a specific invitation that a person created
+   * @param authorization
+   * @param personId
+   * @param person
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @DELETE("people/{personId}/invitation/{invitationId}")
+  Call<Invitation> deleteInvitation(@Header("Authorization") String authorization,
+      @Path("personId") int personId, @Body Person person, @Path("invitationId") int invitationId,
+      @Body Invitation invitation);
+
+  /**
+   * Gets all invitations
+   * @param authorization
+   * @return
+   */
+  @GET("invitations")
+  Call<List<Invitation>> getAllInvitations(@Header("Authorization") String authorization);
+
+  /**
+   * Gets a specific invitation
+   * @param authorization
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @GET("invitations/{invitationId}")
+  Call<Invitation> getInvitation(@Header("Authorization") String authorization,
+      @Path("invitationId") int invitationId, @Body Invitation invitation);
+
+  /**
+   * Gets all people associated with an invitation
+   * @param authorization
+   * @param invitationId
+   * @param invitation
+   * @return
+   */
+  @GET("invitations/{invitationId}/people")
+  Call<List<Invitation>> getPeoplePerInvitation(@Header("Authorization") String authorization,
+      @Path("invitaitonId") int invitationId, @Body Invitation invitation);
 }
--- app/src/main/java/edu/cnm/deepdive/joinme/view/SignInActivity.java
+++ app/src/main/java/edu/cnm/deepdive/joinme/view/SignInActivity.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.joinme.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,8 +10,10 @@ import android.view.View.OnClickListener;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 import edu.cnm.deepdive.joinme.JoinMeApplication;
 import edu.cnm.deepdive.joinme.R;
@@ -19,7 +22,7 @@ import edu.cnm.deepdive.joinme.controller.MainActivity;
 /**
  * The type Sign in activity.
  */
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
   private static final int REQUEST_CODE = 1010;
   /**
    * The Sign in.
@@ -36,39 +39,37 @@ public class SignInActivity extends AppCompatActivity {
     signIn.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
-        signin();
+        signIn();
       }
     });
-
   }
 
   @Override
   protected void onStart() {
-    super.onStart();
     GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
     if(account != null){
       JoinMeApplication.getInstance().setAccount(account);
-      switchToMain();
     }
+    super.onStart();
   }
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-    if(requestCode== REQUEST_CODE){
+    super.onActivityResult(requestCode, resultCode, data);
+    if(requestCode == REQUEST_CODE){
       try {
         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
         account = task.getResult(ApiException.class);
         JoinMeApplication.getInstance().setAccount(account);
       } catch (ApiException e) {
         //e.printStackTrace();
-        Toast.makeText(this,"There was a error logging in", Toast.LENGTH_LONG).show();
       }
       switchToMain();
     }
   }
 
 
-  private void signin(){
+  private void signIn(){
     Intent intent = JoinMeApplication.getInstance().getClient().getSignInIntent();
     startActivityForResult(intent, REQUEST_CODE);
   }
@@ -77,5 +78,10 @@ public class SignInActivity extends AppCompatActivity {
     Intent intent = new Intent(this, MainActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
     startActivity(intent);
+  }
+
+  @Override
+  public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
   }
 }
