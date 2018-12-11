d2e6ed5e03ddc634248939e60482cef7cd5bcc96
Alex Rael <alprael22@gmail.com>
2018-12-11 10:01:34 -0700

    formatted clientdb.sql file

--- ClientDB.sql
+++ ClientDB.sql
@@ -1,9 +1,41 @@
-CREATE TABLE IF NOT EXISTS `${TABLE_NAME}` (`invitation_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `user_sender` INTEGER NOT NULL, `user_receiver` INTEGER NOT NULL, `date` TEXT NOT NULL, `description` TEXT NOT NULL, `title` TEXT NOT NULL, `location` TEXT NOT NULL, `wasDelivered` INTEGER NOT NULL, `willAttend` INTEGER NOT NULL, `degreesRemaining` INTEGER NOT NULL, FOREIGN KEY(`user_sender`) REFERENCES `people`(`person_id`) ON UPDATE NO ACTION ON DELETE NO ACTION ),
-CREATE UNIQUE INDEX `index_invitations_invitation_id` ON `${TABLE_NAME}` (`invitation_id`)
-CREATE UNIQUE INDEX `index_invitations_user_sender` ON `${TABLE_NAME}` (`user_sender`)
-CREATE UNIQUE INDEX `index_invitations_user_receiver` ON `${TABLE_NAME}` (`user_receiver`)
-CREATE TABLE IF NOT EXISTS `${TABLE_NAME}` (`latitude` REAL NOT NULL, `longitude` REAL NOT NULL, `person_id` INTEGER NOT NULL, `display_name` TEXT NOT NULL, `userImageLocation` TEXT, `google_user_id` TEXT, `user_description` TEXT, `isThisMe` INTEGER NOT NULL, `user_image` TEXT, PRIMARY KEY(`person_id`)),
-CREATE UNIQUE INDEX `index_people_person_id` ON `${TABLE_NAME}` (`person_id`)
-CREATE UNIQUE INDEX `index_people_display_name` ON `${TABLE_NAME}` (`display_name`)
-CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT),
-INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, \b1e886c1d9c0fc24a0552f34a661d38d\)
+CREATE TABLE IF NOT EXISTS `invitations` (
+  `invitation_id`       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  `user_sender_id`      INTEGER NOT NULL,
+  `user_receiver_id`    INTEGER NOT NULL,
+  `date`                TEXT NOT NULL,
+  `description`         TEXT NOT NULL,
+  `title`               TEXT NOT NULL,
+  `location`            TEXT NOT NULL,
+  `wasDelivered`        INTEGER NOT NULL,
+  `willAttend`          INTEGER NOT NULL,
+  `degreesRemaining`    INTEGER NOT NULL
+);
+CREATE UNIQUE INDEX `index_invitations_invitation_id`
+                  ON `invitations` (`invitation_id`);
+CREATE UNIQUE INDEX `index_invitations_user_sender_id`
+                  ON `invitations` (`user_sender_id`)
+CREATE UNIQUE INDEX `index_invitations_user_receiver_id`
+                  ON `invitations` (`user_receiver_id`)
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `latitude`            REAL NOT NULL,
+  `longitude`           REAL NOT NULL,
+  `person_id`           INTEGER NOT NULL,
+  `display_name`        TEXT NOT NULL,
+  `userImageLocation`   TEXT,
+  `google_user_id`      TEXT,
+  `user_description`    TEXT,
+  `isThisMe`            INTEGER NOT NULL,
+  `user_image`          TEXT,
+                        PRIMARY KEY(`person_id`)
+);
+CREATE UNIQUE INDEX `index_people_person_id`
+                  ON `people` (`person_id`)
+CREATE UNIQUE INDEX `index_people_display_name`
+                  ON `people` (`display_name`);
+
+CREATE TABLE IF NOT EXISTS room_master_table (
+  id                    INTEGER PRIMARY KEY,
+  identity_hash         TEXT),
+                        INSERT OR REPLACE INTO room_master_table (id,identity_hash)
+                        VALUES(42, "0d6bb583c9f9c38d53244ca3f75107a9")
